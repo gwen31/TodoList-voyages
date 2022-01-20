6495c3b310f86fbdbb6202493eeca89d6f48e495
gwen31 <merlegwenaelle026@gmail.com>
2022-01-20 15:17:02 +0100

    installation-configuration

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+
+
+const port = process.env.PORT || 5000;
+
+// pre-route middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+//require('./routes')(app);
+
+const server = app.listen(port, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Server listening on port ${port}`);
+    }
+});
+module.exports = server;
